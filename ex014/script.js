82d765de24a6610906e6b94552db413a05c98449
Marcio Tanan <marcio-tanan@hotmail.com>
2023-02-02 21:19:42 -0300

    update

--- ex014/script.js
+++ ex014/script.js
@@ -2,20 +2,21 @@ function carregar() {
   var msg = document.getElementById("msg")
   var img = document.getElementById("imagem")
   var data = new Date()
-  var hora = data.getHours()
+  var hora =00 //data.getHours()
   msg.innerHTML = `Agora são ${hora} horas`
   
   
-  if (hora >= 0 && hora < 12) {
+  if (hora >= 6 && hora < 12) {
     img.src = "manha.png"
- 
+    document.body.style.background = '#e2cd9f'
  
   }else if (hora >= 12 && hora <= 18) {
       img.src = "tarde.png"
-    
+      document.body.style.background = '#b9846f'
     
      } else {
         img.src = "noite.png"
+        document.body.style.background = '#515154'
       }
     }
   
