14bd4e536bedd87b9102a858ad4bad7044efc355
Marcio Tanan <marcio-tanan@hotmail.com>
2023-02-04 20:24:12 -0300

    vetores

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,3 +1,11 @@
 let num = [5,8,2,9,3]
 
+console.log(num)
+num.push(1)
 console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num[0])
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+num.sort()
+console.log(num)
+
