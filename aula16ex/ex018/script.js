2174fcda115d43f255fd467b5cad7e4d0cb096e4
Marcio Tanan <marcio-tanan@hotmail.com>
2023-02-05 15:17:43 -0300

    update

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,6 +1,6 @@
 let num = document.querySelector("input#fnum")
 let lista = document.querySelector("select#flista")
-let res = document.querySelector("div#res")
+let res = document.getElementById("res")
 let valores = []
 
 function isNumero(n) {
@@ -26,10 +26,21 @@ function adicionar(){
     let item = document.createElement("option") 
     item.text = `Valor ${num.value} adicionado`
     lista.appendChild(item)
+    res.innerHTML = ""
   }else {
     alert("Valor inválido ou já encontrado na lista")
   }
   num.value = ""
   num.focus()
+}
+
+function finalizar() {
+  if (valores.length == 0) {
+    alert("Adicione valores antes de finalizar!")
+  }else{
+    let tot = valores.length
+    res.innerHTML = ""
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+  }
 }
 
